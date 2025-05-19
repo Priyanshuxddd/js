6d61041473408218028a43b3512e12dee6af45db
Priyanshu Saxena <priyanshusaxena221@gmail.com>
2025-05-19 18:29:45 +0530

    everything done with conversion

--- conversion_operation.js
+++ conversion_operation.js
@@ -11,12 +11,47 @@ console.log(valueInNumber);
 
 */
 let isLoggedIn ="hlo "
+/*
 
  BooleanisLoggedIn= Boolean(isLoggedIn)
 console.log(BooleanisLoggedIn);
 
+*/
+/*
 
 let someNumber= 33
 let stringNumber= String(someNumber)
 console.log(someNumber);
 console.log(typeof someNumber);
+ */
+//******************operations ********************
+
+
+/*
+
+
+let value =3
+let negValue = -value
+console.log(negValue);
+console.log(2+2);
+console.log(2**3);
+
+*/
+
+/*
+ let str1="hello"
+let str2="natsuo"
+
+let str3=str1+str2
+
+console.log(str3)
+
+*/
+
+
+//   console.log(+true);
+
+
+let gameCounter= 100
+++gameCounter
+console.log(gameCounter);
