2d985c458c57f612931034322f99b5636927b47a
Priyanshu Saxena <priyanshusaxena221@gmail.com>
2025-05-20 00:29:33 +0530

    done with string

---
+++ 05_string.js
@@ -0,0 +1,45 @@
+//not recommended
+
+
+/*
+
+
+const name="priyanshu"
+const repoCount=50
+
+// console.log(name+repoCount+"Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName= new String('Natsuo')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+
+//baaki strings ke methods ke liye mdn dekho
+
+
+const newString= "   natsuo    "
+console.log(newString);
+console.log(newString.trim());
+
+
+*/
+
+
+
+const url ="https://lol.com/lol%20code"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('lol'));
+
+
+const oneString = "Natsu-ocook-s"
+console.log(oneString.split('-'));
+
+
