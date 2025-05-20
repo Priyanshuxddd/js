75622b6cafd744fd2f2159253b8e1868dbed6a3e
Priyanshu Saxena <priyanshusaxena221@gmail.com>
2025-05-20 20:01:21 +0530

    arrays done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,50 @@
+//array 
+
+const myArr= [0,1,2,3,4,5]
+const myHeroes= ["ironman", "Captain"]
+
+
+const myArr2= new Array(1,2,3,1)
+
+// console.log(myArr[0]);
+
+
+// ++++++++++++++ array methods ++++++++++++++++++
+
+// myArr.push(6)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+//console.log(myArr.includes(2));
+/*
+
+const newArr = myArr.join()
+
+
+console.log(myArr);
+console.log(typeof newArr);
+
+*/
+
+
+//+++++++slice, splice +++++++++++
+
+console.log("a",myArr);
+
+const my1= myArr.slice(1,3)
+
+
+
+console.log(my1);
+console.log("b",myArr);
+
+
+
+const my2= myArr.splice(1,3)
+console.log("c",myArr);
+console.log(my2);
+
+
+
