412e1f5aa2d110b3d6929b5c1cf09ace3ee8ccea
Priyanshu Saxena <priyanshusaxena221@gmail.com>
2025-05-23 15:20:55 +0530

    controlling done

--- 04_control_flow/01.js
+++ 04_control_flow/01.js
@@ -17,7 +17,7 @@ if(temperature === 59 ){
 */
 
 
-// < , > , <= , >= , == , === , !=
+// < , > , <= , >= , == , === , != , !==
 
 /*
 
@@ -76,9 +76,33 @@ if(loggedInFromEmail || loggedInFromGoogle){
 
 }
 
+//Nullish Coalescing Operator (??): null undefined
+
+let val1 ;
+// val1 = 5 ?? 10
+
+//   val1 = null ?? 10
+
+// val1 = undefined ?? 15
+
+val1 = null ?? 10 ?? 20
+
+
+console.log(val1);
+
+
+// terniary operator
+
+//Condition ? true : false 
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
 
 
 
+
+//&& means saari condition true honi chiy e
+// || means ek bhi true ho to kaam kregi
 
 
 
---
+++ 04_control_flow/03_truthy.js
@@ -0,0 +1,29 @@
+/*
+
+
+const userEmail = []
+if (userEmail) {
+        console.log("Got User Email");
+        
+} else {
+    console.log("doesnt have email ");
+    
+}
+
+console.log(userEmail)
+
+if (userEmail.length === 0 ){
+    console.log("array is empty");
+    
+
+}
+
+*/
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("object is empty");
+    
+    
+}
