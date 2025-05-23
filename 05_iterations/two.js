fcfadbfb1ab4ac802d9e2a12475a634f10546158
Priyanshu Saxena <priyanshusaxena221@gmail.com>
2025-05-23 17:42:06 +0530

    do while and while loops done too cuh

---
+++ 05_iterations/two.js
@@ -0,0 +1,33 @@
+
+let index = 0
+while (index<=10) {
+//    console.log(`Value of index is ${index}`);
+    
+    index = index +2
+}
+
+
+
+
+
+let myArray = ["Subscription", "Sundew", "Horra", "Natsuo"]
+
+let arr= 0
+
+while (arr < myArray.length) {
+//    console.log(`heyxdd ${myArray[arr]}`);
+  
+    arr = arr+1
+}
+
+
+//do while me print pehle hojata hai fir condition check hogi
+
+
+let score = 11
+do {
+    
+    console.log(`Score is ${score}`);
+    score++
+
+} while (score <=10);
